4f555456e0bde88d7ca85177cf5e2503dbb7c1aa
Raaed Serag <raaedserag@gmail.com>
2020-11-16 12:52:00 -0800

    Remove extra consol.log

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ redisMainConnection.init()
 
 //Routes
 require("./app/routes")(app);
-console.log(JSON.stringify(require("./app/config")))
+
 // Initiate the server on the selected host:port
 app.listen(port, host, () => winston.info(`Server started as ${process.env.NODE_ENV} on ${host}:${port}`));
 
