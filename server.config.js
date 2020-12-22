c530346417807d40bac05c220ce96a80bbee8b2f
Raaed Serag <raaedserag@gmail.com>
2020-12-22 12:35:08 +0200

    Fix pm2 config

--- server.config.js
+++ server.config.js
@@ -1,17 +1,8 @@
-var pm2Config; 
-if (process.env.NODE_ENV == "production" || "dev-prod") pm2Config = {
+var pm2Config = {
   apps : [{
     name      : 'BI-Locations',
     script    : 'index.js',
   }]
 }
 
-
-else pm2Config = {
-  apps : [{
-    name      : 'BI-Locations',
-    script    : 'index.js',
-    node_args: '-r dotenv/config'
-  }]
-}
 module.exports = pm2Config
