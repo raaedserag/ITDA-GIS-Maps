f489d75506e92e9ff77d7da93559a05c0d7e3781
Raaed Serag <raaedserag@gmail.com>
2020-12-22 13:17:28 +0200

    Dev==prod

--- app/logging.js
+++ app/logging.js
@@ -28,15 +28,14 @@ module.exports = function () {
   // use a log file and a database to log errors.
   winston.add(new winston.transports.File({ filename: "logfile.log" }));
   //  Use Console logging in development mode only
-  if (process.env.NODE_ENV == "development" ) {
-    winston.add(
-      new winston.transports.Console({
-        level: "info",
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.simple()
-        ),
-      })
-    );
-  }
+  winston.add(
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    })
+  );
+
 };
--- app/routes.js
+++ app/routes.js
@@ -25,9 +25,8 @@ module.exports = function (app) {
   }));
  
   // Apply Morgan middleware in development mode
-  if (process.env.NODE_ENV == "development" || process.env.NODE_ENV == "staging") {
-    app.use(require("morgan")("tiny"));
-  }
+  app.use(require("morgan")("tiny"));
+  
   
   // Apply Routes
   app.use("/admin", adminApi);
